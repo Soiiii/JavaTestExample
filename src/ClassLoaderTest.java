8b5f8c91353cd143222ec49c94eba14dc430e852
Soiiii <cojomkr33@gmail.com>
2025-03-04 17:23:08 +0900

    Java util Test example

--- src/ClassLoaderTest.java
+++ src/ClassLoaderTest.java
@@ -2,11 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.Module;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class ClassLoaderTest {
-    public void testGetResource() {
+    public static void main(String[] args) {
+
+        ClassLoaderTest test = new ClassLoaderTest();
+//        test.getResource();
+//        test.getResources();
+//        test.getResourceAsStream();
+        test.getClassLoaderName();
+        test.checkParallelCapable();
+//        test.getUnnamedModule();
+//        test.getDefinedPackage();
+//        test.getDefinedPackages();
+//        test.getPlatformClassLoader();
+//        test.findResources();
+
+    }
+    public void getResource() {
         ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
         URL resourceUrl = classLoader.getResource("sample.txt");
 
@@ -17,7 +35,7 @@ public class ClassLoaderTest {
         }
     }
 
-    public void testGetResources() {
+    public void getResources() {
         ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
         try {
             Enumeration<URL> resources = classLoader.getResources("config.properties");
@@ -35,7 +53,7 @@ public class ClassLoaderTest {
         }
     }
 
-    public void testGetResourceAsStream() {
+    public void getResourceAsStream() {
         ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
         try (InputStream inputStream = classLoader.getResourceAsStream("sample.txt")) {
             if (inputStream == null) {
@@ -54,10 +72,60 @@ public class ClassLoaderTest {
         }
     }
 
-    public static void main(String[] args) {
-        ClassLoaderTest test = new ClassLoaderTest();
-        test.testGetResource();
-        test.testGetResources();
-        test.testGetResourceAsStream();
+    // 3. isRegisteredAsParallelCapable()
+    public void checkParallelCapable() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        boolean isParallelCapable = classLoader.isRegisteredAsParallelCapable();
+        System.out.println("Is registered as parallel capable: " + isParallelCapable);
+    }
+
+    // 4. getUnnamedModule()
+    public void getUnnamedModule() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        Module unnamedModule = classLoader.getUnnamedModule();
+        System.out.println("Unnamed Module: " + unnamedModule.getName());
+    }
+
+    // 5. getDefinedPackage(String arg0)
+    public void getDefinedPackage() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        Package pkg = classLoader.getDefinedPackage("java.lang");
+        if (pkg != null) {
+            System.out.println("Defined package: " + pkg.getName());
+        } else {
+            System.out.println("Package not found!");
+        }
+    }
+
+    // 6. getDefinedPackages()
+    public void getDefinedPackages() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        Package[] packages = classLoader.getDefinedPackages();
+        System.out.println("Defined Packages:");
+        for (Package pkg : packages) {
+            System.out.println(pkg.getName());
+        }
+    }
+
+    // 7. getPlatformClassLoader()
+    public void getPlatformClassLoader() {
+        ClassLoader platformClassLoader = ClassLoader.getPlatformClassLoader();
+        System.out.println("Platform ClassLoader: " + platformClassLoader);
+    }
+
+    // 8. resources(String name)
+    public void findResources() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        Stream<URL> resourceStream = classLoader.resources("META-INF/MANIFEST.MF");
+
+        System.out.println("Resources found:");
+        resourceStream.forEach(System.out::println);
+    }
+
+    // 9. getName()
+    public void getClassLoaderName() {
+        ClassLoader classLoader = ClassLoaderTest.class.getClassLoader();
+        Optional<String> name = Optional.ofNullable(classLoader.getName());
+        System.out.println("ClassLoader Name: " + name.orElse("Unnamed ClassLoader"));
     }
 }

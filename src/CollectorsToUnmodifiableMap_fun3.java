71a679e1fc897e2db0aacd170bf1043c3531a724
Soiiii <cojomkr33@gmail.com>
2025-03-04 17:23:30 +0900

    Java util Test example

--- src/CollectorsToUnmodifiableMap_fun3.java
+++ src/CollectorsToUnmodifiableMap_fun3.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class toUnmodifiableMap_fun3 {
+public class CollectorsToUnmodifiableMap_fun3 {
     public static void main(String[] args) {
 
         List<String> fruits = List.of("apple", "banana", "cherry", "apricot", "blueberry");
